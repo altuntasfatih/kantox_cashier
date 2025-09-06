40ee676ec91ef34b6fa0a4040c5fc8b79207c815
Fatih Altuntas <altuntasfatih42@gmail.com>
2025-09-07 02:44:02 +0300

    fix: fix config

--- lib/kantox_cashier/shopping_cart/product.ex
+++ lib/kantox_cashier/shopping_cart/product.ex
@@ -10,9 +10,9 @@ defmodule KantoxCashier.Product do
   def green_tea, do: %__MODULE__{code: :GR1, price: price(:GR1)}
   def strawberry, do: %__MODULE__{code: :SR1, price: price(:SR1)}
 
-  def price(:CF1), do: Application.get_env(:kantox_cashier, :basket)[:CF1]
-  def price(:GR1), do: Application.get_env(:kantox_cashier, :basket)[:GR1]
-  def price(:SR1), do: Application.get_env(:kantox_cashier, :basket)[:SR1]
+  def price(:CF1), do: Application.get_env(:kantox_cashier, :products)[:CF1]
+  def price(:GR1), do: Application.get_env(:kantox_cashier, :products)[:GR1]
+  def price(:SR1), do: Application.get_env(:kantox_cashier, :products)[:SR1]
 
   def code_to_string(:CF1), do: "Coffee"
   def code_to_string(:GR1), do: "Green Tea"
