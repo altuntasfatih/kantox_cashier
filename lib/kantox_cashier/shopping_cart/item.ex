460b8ff6d6f543d25bc85cbe5921f2ee114b4098
Fatih Altuntas <altuntasfatih42@gmail.com>
2025-09-07 14:11:25 +0300

    refactor: remove redundant functions

--- lib/kantox_cashier/shopping_cart/item.ex
+++ lib/kantox_cashier/shopping_cart/item.ex
@@ -6,13 +6,9 @@ defmodule KantoxCashier.Item do
   def new(:SR1), do: strawberry()
   def new(_), do: {:error, :invalid_item_code}
 
-  def coffee, do: %__MODULE__{code: :CF1, price: price(:CF1)}
-  def green_tea, do: %__MODULE__{code: :GR1, price: price(:GR1)}
-  def strawberry, do: %__MODULE__{code: :SR1, price: price(:SR1)}
-
-  def price(:CF1), do: config(:CF1)[:price]
-  def price(:GR1), do: config(:GR1)[:price]
-  def price(:SR1), do: config(:SR1)[:price]
+  def coffee, do: %__MODULE__{code: :CF1, price: config(:CF1)[:price]}
+  def green_tea, do: %__MODULE__{code: :GR1, price: config(:GR1)[:price]}
+  def strawberry, do: %__MODULE__{code: :SR1, price: config(:SR1)[:price]}
 
   def code_to_string(:CF1), do: config(:CF1)[:name]
   def code_to_string(:GR1), do: config(:GR1)[:name]
