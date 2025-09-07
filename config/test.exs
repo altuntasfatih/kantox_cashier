de3e10a1ce0147da21c89ec1dc1f25dd0e40eeb9
Fatih Altuntas <altuntasfatih42@gmail.com>
2025-09-07 03:10:28 +0300

    refactor: format

--- config/test.exs
+++ config/test.exs
@@ -4,7 +4,10 @@ config :kantox_cashier, :campaigns, [
   {KantoxCashier.Campaign.BulkPurchaseCoffee,
    enabled: true, coffee_count_threshold: 3, campaigns_amount: 3.75, name: "Bulk Purchase Coffee"},
   {KantoxCashier.Campaign.BulkPurchaseStrawberry,
-   enabled: true, strawberry_count_threshold: 3, campaigns_amount: 0.50, name: "Bulk Purchase Strawberry"},
+   enabled: true,
+   strawberry_count_threshold: 3,
+   campaigns_amount: 0.50,
+   name: "Bulk Purchase Strawberry"},
   {KantoxCashier.Campaign.BuyOneGetOneFreeGreentea,
    enabled: true, green_tea_count_threshold: 2, name: "Buy One Get One Free Green Tea"}
 ]
